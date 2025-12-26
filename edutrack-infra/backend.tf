e15d9aaa1cd877d4ac072d39e7cef8c9cc816af8
delliganesh-nt10 <delliganesh.a@navattech.com>
2025-12-26 12:19:41 +0530

    updated

--- edutrack-infra/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "edutrack-terraform-state-2703"
-    key            = "edutrack/infra.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
--- edutrack-infra/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "aws_region" {
-  description = "AWS region for EduTrack LMS"
-  type        = string
-  default     = "us-east-1"
-}
