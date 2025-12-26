cf44fb82e79d492bbb0c9fd166f83515e62727a2
delliganesh-nt10 <delliganesh.a@navattech.com>
2025-12-26 15:01:05 +0530

    infra minor update

--- edutrack-infra/backend.tf
+++ edutrack-infra/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "edutrack-terraform-state-2703"
+    bucket         = "edutrack-terraform-state-0327"
     key            = "edutrack/infra.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
+    dynamodb_table = "terraform-infra-locks"
     encrypt        = true
   }
 }
